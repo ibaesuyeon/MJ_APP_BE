6e48c55873032aa047e50abbc7c2c7c8e88434d7
suyeon <ibaesuyeon@Naver.com>
2023-06-26 05:38:33 +0900

    [Chore] 초기파일

---
+++ src/main/java/com/example/MJ_App_BE/MjAppBeApplication.java
@@ -0,0 +1,14 @@
+package com.example.MJ_App_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjAppBeApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
