30fece1ef987c33bcd5180f98201fb3bdddcd6d1
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-08-03 19:52:54 +0900

    [Feat] Keyword Create/Read/Delete 기능 구현

--- src/main/java/com/example/MJ_App_BE/controller/KeywordController.java
+++ src/main/java/com/example/MJ_App_BE/controller/KeywordController.java
@@ -1,8 +1,14 @@
 package com.example.MJ_App_BE.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.MJ_App_BE.Result.CommonResult;
+import com.example.MJ_App_BE.Result.ListResult;
+import com.example.MJ_App_BE.Result.ResponseService;
+import com.example.MJ_App_BE.data.dto.keyworddto.KeywordDto;
+import com.example.MJ_App_BE.service.KeywordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 
@@ -11,8 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class KeywordController {
 
+    private final KeywordService keywordService;
+    private final ResponseService responseService;
+
+    @Autowired
+    public KeywordController(KeywordService keywordService, ResponseService responseService) {
+        this.keywordService = keywordService;
+        this.responseService = responseService;
+    }
+
     // 키워드 생성
-    // 키워드 수정
-    // 키워드 삭제 - 태그랑 다르게 개별 삭제
+    @PostMapping("/register")
+    public CommonResult saveKeyword(@RequestBody KeywordDto keywordDto) {
+        keywordService.saveKeyword(keywordDto);
+        return responseService.getSuccessfulResult();
+    }
+
+    // 키워드 수정(개별 삭제-저장하면 필요없을 듯?)
+    // 키워드 삭제 - 개별 삭제
+    @DeleteMapping("/deletion/{keywordId}")
+    public CommonResult deleteKeyword(@PathVariable Long keywordId) {
+        keywordService.deleteKeyword(keywordId);
+        return responseService.getSuccessfulResult();
+    }
+
     // 키워드 리스트로 조회
+    @GetMapping("/{userId}")
+    public ListResult<KeywordDto> getKeywords(@PathVariable Long userId) {
+        List<KeywordDto> keywords = keywordService.getKeywords(userId).getKeywordDtoList();
+        return responseService.getListResult(keywords);
+    }
 }
--- src/main/java/com/example/MJ_App_BE/service/KeywordService.java
+++ src/main/java/com/example/MJ_App_BE/service/KeywordService.java
@@ -1,4 +1,12 @@
 package com.example.MJ_App_BE.service;
 
+import com.example.MJ_App_BE.data.dto.keyworddto.KeywordDto;
+import com.example.MJ_App_BE.data.dto.keyworddto.KeywordResponseDto;
+
 public interface KeywordService {
+
+    void saveKeyword(KeywordDto keywordDto);
+    // KeywordResponseDto changeKeyword();
+    void deleteKeyword(Long keywordId);
+    KeywordResponseDto getKeywords(Long userId);
 }
